cfbe8f8efed0a80ef0fc2aa4deb5deb194e1f38a
KEERTHANA910 <31275974+KEERTHANA910@users.noreply.github.com>
2017-08-30 17:19:40 +0530

    Add files via upload

---
+++ 3.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b,c,d,e,f;
+cout<<"first number=";
+cin>>a;
+cout<<"second number=";
+cin>>b;
+c=a+b;
+cout<<"addition="<<c;
+d=a-b;
+cout<<"subtraction="<<d;
+e=a*b;
+cout<<"product="<<e;
+f=a/b;
+cout<<"division="<<f;
+}
+
+
+
+
---
+++ 6.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b,c,d;
+cout<<"radius=";
+cin>>a;
+b=2*a;
+cout<<"diameter="<<b;
+c=2*3.14*a;
+cout<<"circumferance="<<c;
+d=3.14*a*a;
+cout<<"area="<<d;
+}
+
